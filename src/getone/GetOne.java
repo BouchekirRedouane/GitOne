b91cfb91c09228d5a09dee9ba6a8517f59e1922b
redOne <redouane.bouchekir@gmail.com>
2022-02-03 13:42:14 +0100

    Third commit

--- src/getone/GetOne.java
+++ src/getone/GetOne.java
@@ -15,6 +15,8 @@ public class GetOne {
      */
     public static void main(String[] args) {
         System.out.println("FIRST CHANGE");
+        
+        System.out.println("SECOND CHANGES");
     }
     
 }
