2ec07bba9b45a33d55310372cd8eb29ed8ef2376
redOne <redouane.bouchekir@gmail.com>
2022-02-03 13:37:48 +0100

    first commit

---
+++ src/getone/GetOne.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package getone;
+
+/**
+ *
+ * @author redouane
+ */
+public class GetOne {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
